1782e0516a2a182c82ad39900ff8714bf458519e
Curtis Timson <curt@live.co.uk>
2022-05-12 17:49:59 +0100

    🎉 Initial setup (#1)
    * 🎉 Initial setup
    
    * stop preventing deletion

---
+++ src/getFilesInDir.ts
@@ -0,0 +1,29 @@
+import { walk } from 'https://deno.land/std@0.138.0/fs/mod.ts';
+import { IFile } from "./IFile.ts";
+import { daysUntilDelete } from './config.ts';
+
+const now:Date = new Date();
+const delThreshold: Date = new Date(now);
+delThreshold.setDate(delThreshold.getDate() - daysUntilDelete);
+
+export const getFilesInDir = async (directory: string) : Promise<IFile[]> => {
+  const files: IFile[] = [];
+
+    for await (const dirEntry of walk(directory)) {
+    if (dirEntry.isFile) {
+      const fileInfo = await Deno.stat(dirEntry.path);
+  
+      const file : IFile = {
+        fileName: dirEntry.name,
+        modifiedDate: fileInfo.mtime,
+        path: dirEntry.path,
+        size: fileInfo.size,
+        toBeDeleted: fileInfo.mtime ? fileInfo.mtime.getTime() < delThreshold.getTime() : false,
+      }
+  
+      files.push(file);
+    }
+  }
+
+  return files;
+}
