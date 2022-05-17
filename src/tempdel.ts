7c719a6326f3ef2456b8968609fd748a662135e1
Curtis Timson <curt@live.co.uk>
2022-05-17 08:14:42 +0100

    👷‍♀️ CI for linting & formatting (#2)

--- src/tempdel.ts
+++ src/tempdel.ts
@@ -1,26 +1,36 @@
-import { Table } from "https://deno.land/x/cliffy@v0.16.0/table/mod.ts";
-import { getFilesInDir } from './getFilesInDir.ts'
-import { IFile } from "./IFile.ts";
+import { Table } from 'https://deno.land/x/cliffy@v0.16.0/table/mod.ts';
+import { getFilesInDir } from './getFilesInDir.ts';
+import { IFile } from './IFile.ts';
 import { daysUntilDelete } from './config.ts';
-import { deleteFiles } from "./deleteFiles.ts";
+import { deleteFiles } from './deleteFiles.ts';
 
-const getTotalFileSize = (files: IFile[]): number => files.map(f => f.size).reduce((x,y) => x + y, 0);
-const formatBytesToMB = (bytes: number): string => (bytes / 1024 / 1024).toFixed(2);
+const getTotalFileSize = (files: IFile[]): number =>
+  files.map((f) => f.size).reduce((x, y) => x + y, 0);
+const formatBytesToMB = (bytes: number): string =>
+  (bytes / 1024 / 1024).toFixed(2);
 
 const logFilesToBeDeleted = (filesToBeDeleted: IFile[]) => {
-  console.log('Files to be deleted:')
+  console.log('Files to be deleted:');
 
   if (filesToBeDeleted.length > 100) {
-    filesToBeDeleted.forEach(file => {
-      console.log(`${file.path} | ${file.modifiedDate!.toDateString()} | ${formatBytesToMB(file.size)}`);
-    })
-  }
-  else {
+    filesToBeDeleted.forEach((file) => {
+      console.log(
+        `${file.path} | ${file.modifiedDate!.toDateString()} | ${
+          formatBytesToMB(file.size)
+        }`,
+      );
+    });
+  } else {
+    // deno-lint-ignore no-explicit-any
     const tbody: any[] = [];
     let i = 0;
-    filesToBeDeleted.forEach(file => {
+    filesToBeDeleted.forEach((file) => {
       if (i < 100) {
-        tbody.push([file.path, file.modifiedDate!.toDateString(), formatBytesToMB(file.size)])
+        tbody.push([
+          file.path,
+          file.modifiedDate!.toDateString(),
+          formatBytesToMB(file.size),
+        ]);
         i = i + 1;
       }
     });
@@ -31,7 +41,7 @@ const logFilesToBeDeleted = (filesToBeDeleted: IFile[]) => {
       .border(true)
       .render();
   }
-}
+};
 
 const main = async () => {
   console.log('----------');
@@ -39,24 +49,30 @@ const main = async () => {
 
   console.log('getting file information...');
   const files = await getFilesInDir('../../temp');
-  const totalFileSize = getTotalFileSize(files)
+  const totalFileSize = getTotalFileSize(files);
   const totalFileSizeMb = formatBytesToMB(totalFileSize);
 
-  const filesToBeDeleted = files.filter(f => f.toBeDeleted)
-  const filesToBeDeletedFileSize = getTotalFileSize(filesToBeDeleted)
+  const filesToBeDeleted = files.filter((f) => f.toBeDeleted);
+  const filesToBeDeletedFileSize = getTotalFileSize(filesToBeDeleted);
   const filesToBeDeletedFileSizeMb = formatBytesToMB(filesToBeDeletedFileSize);
 
-  const percentageToBeDeleted = ((filesToBeDeletedFileSize / totalFileSize) * 100).toFixed(2);
+  const percentageToBeDeleted =
+    ((filesToBeDeletedFileSize / totalFileSize) * 100).toFixed(2);
 
   if (filesToBeDeleted.length) {
-    logFilesToBeDeleted(filesToBeDeleted);  
+    logFilesToBeDeleted(filesToBeDeleted);
   }
 
   new Table()
     .header(['', 'File Count', 'File Size (MB)', '%'])
     .body([
       ['All files', files.length, totalFileSizeMb, 100],
-      ['To be deleted', filesToBeDeleted.length, filesToBeDeletedFileSizeMb, percentageToBeDeleted]
+      [
+        'To be deleted',
+        filesToBeDeleted.length,
+        filesToBeDeletedFileSizeMb,
+        percentageToBeDeleted,
+      ],
     ]).border(true).render();
 
   if (!filesToBeDeleted.length) {
@@ -64,9 +80,13 @@ const main = async () => {
     return;
   }
 
-  if (confirm(`Do you wish to delete the ${filesToBeDeleted.length} files that are older than ${daysUntilDelete} days?`)) {
+  if (
+    confirm(
+      `Do you wish to delete the ${filesToBeDeleted.length} files that are older than ${daysUntilDelete} days?`,
+    )
+  ) {
     await deleteFiles(filesToBeDeleted);
-  };
-}
+  }
+};
 
 await main();
